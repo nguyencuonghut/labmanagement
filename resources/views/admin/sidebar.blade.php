1d312e2da4dee66112e8b560ed962d17b5b88539
Tony Nguyen <nguyencuonghut55@gmail.com>
2017-08-19 11:17:35 +0700

    Add the Laravel 5.4 and AdminLTE

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,55 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="active treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-industry"></i>
+                    <span>Thiết bị</span>
+                    <span class="pull-right-container">
+                         <i class="fa fa-angle-left pull-right"></i>
+                         </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/charts/chartjs.html"><i class="fa fa-tint"></i> Hóa chất</a></li>
+                    <li><a href="#"><i class="fa fa-flask"></i> Dụng cụ</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-bar-chart"></i>
+                    <span>Báo cáo</span>
+                    <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> Báo cáo hoạt động</a></li>
+                    <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Báo cáo tài sản hóa chất</a></li>
+                    <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Báo cáo tài sản dụng cụ</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/users/signup.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('title', 'Sign up')
+
+@section('content')
+    <!-- Create a form to register -->
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <h1 style="text-align: center">Register now</h1>
+        </div>
+    </div>
+@endsection
